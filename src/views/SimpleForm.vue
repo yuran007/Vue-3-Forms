d67234247d276ef6611fbfaa67a60648f5eeea98
Yuriy Titov <yuranus@gmail.com>
2021-07-24 15:40:29 +0300

    complete l8-start

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form>
+    <form @submit.prevent="sendForm">
       <BaseSelect
         :options="categories"
         v-model="event.category"
@@ -62,6 +62,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
@@ -89,6 +91,20 @@ export default {
         { label: 'Yes', value: 1 },
         { label: 'No', value: 0 }
       ]
+    }
+  },
+  methods: {
+    sendForm () {
+      axios.post(
+        'http://my-json-server.typicode.com/yuran007/Vue-3-Forms/events',
+        this.event
+      )
+        .then(function (response) {
+          console.log('Response', response)
+        })
+        .catch(function (err) {
+          console.log('Error', err)
+        })
     }
   }
 }
